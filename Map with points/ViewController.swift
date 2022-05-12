190db45f45960321f7e4fd1ba2d297344e8525e1
Артём Симан <neonx@MacBook-Air-Artem.local>
2022-05-12 14:52:28 +0300

    Initial commit

---
+++ Map with points/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Map with points
+//
+//  Created by Артём Симан on 12.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
